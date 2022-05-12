f531853a4a8686b1112d0b9411b9a7fc24e06b77
willtylerjr <willtylerjr@outlook.com>
2022-05-12 00:42:54 -0700

    Level Select Implemented
    Level Select UI (bottom right of screen) is now implemented fully. Each button (numbered 1, 2, 3) respectively corresponds to loading that scene which represents levels 1, 2, and 3. Each scene has that level select, so one can navigate to and from each level to each level. This includes its own level, which functionally serves as a level restart.

---
+++ RollABall/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public Button level1;
+    public Button level2;
+    public Button level3;
+
+    public void loadLevel1()
+    {
+        SceneManager.LoadScene("Rollaball-Level1");
+    }
+
+    public void loadLevel2()
+    {
+        SceneManager.LoadScene("Rollaball-Level2");
+    }
+
+    public void loadLevel3()
+    {
+        SceneManager.LoadScene("Rollaball-Level3");
+    }
+}
